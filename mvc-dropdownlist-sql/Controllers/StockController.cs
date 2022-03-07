a9dbf261210adf5aeb6fa67394439f2b5bbddae5
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2022-03-07 16:56:03 +0000

    WIP

--- mvc-dropdownlist-sql/Controllers/StockController.cs
+++ mvc-dropdownlist-sql/Controllers/StockController.cs
@@ -12,7 +12,30 @@ namespace mvc_dropdownlist_sql.Controllers
         // GET: Stock
         public ActionResult AddOrEdit(int id = 0)
         {
-            Stock
+            Stock stockModel = new Stock();
+            using (DbModels db = new DbModels())
+            {
+                if (id != 0)
+                    stockModel = db.Stocks.Where(x => x.StockId == id).FirstOrDefault();
+
+                stockModel.ProductCollection = db.Products.ToList<Product>();
+            }
+            
+            // Hard coding dropdown values
+            //stockModel.ProductCollection = new List<Product>() {
+            //new Product() { ProductId = 1, ProductName = "Computer" },
+            //new Product() {ProductId = 2, ProductName = "Mobile Phone" }
+            //};
+
+            return View(stockModel);
+        }
+
+        // POST: Stock
+        [HttpPost]
+        public ActionResult AddOrEdit(Stock stock)
+        {
+            // TODO: Save product ID & count in stock table
+
             return View();
         }
     }
--- mvc-dropdownlist-sql/Models/Stock.cs
+++ mvc-dropdownlist-sql/Models/Stock.cs
@@ -2,11 +2,14 @@ namespace mvc_dropdownlist_sql.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations.Schema;
 
     public partial class Stock
     {
         public int StockId { get; set; }
+
+        [DisplayName("Product")]
         public int ProductId { get; set; }
         public Nullable<int> Count { get; set; }
 
